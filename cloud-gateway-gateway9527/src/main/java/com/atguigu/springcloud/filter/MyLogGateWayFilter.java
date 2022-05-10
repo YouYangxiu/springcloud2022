d505b83394cb2a94a1db784d8d1cd4d2854b8e9e
youyangxiu <yyx1824665362@gmail.com>
2022-05-10 14:17:31 +0800

    5.10第一次提交，完成服务网关的配置

---
+++ cloud-gateway-gateway9527/src/main/java/com/atguigu/springcloud/filter/MyLogGateWayFilter.java
@@ -0,0 +1,32 @@
+package com.atguigu.springcloud.filter;
+
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import java.util.Date;
+
+@Component
+public class MyLogGateWayFilter implements GlobalFilter, Ordered {
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        System.out.println("*********come in MyLogGateWayFilter:" + new Date());
+        String name = exchange.getRequest().getQueryParams().getFirst("name");
+        if (name == null) {
+            System.out.println("用户名为null，非法用户😭");
+            exchange.getResponse().setStatusCode(HttpStatus.NOT_ACCEPTABLE);
+            return exchange.getResponse().setComplete();
+        }
+        return chain.filter(exchange);
+    }
+
+    @Override
+    public int getOrder() {
+        return 0;
+    }
+}
